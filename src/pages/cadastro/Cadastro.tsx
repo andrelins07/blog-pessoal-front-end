414a4de2bab19118f34abac3978818c8def1d8a8
Andre Lins <andrelins2403@hotmail.com>
2025-01-24 06:28:45 -0300

    Implementação da logica de login do usuario

--- src/pages/cadastro/Cadastro.tsx
+++ src/pages/cadastro/Cadastro.tsx
@@ -141,9 +141,8 @@ function Cadastro() {
             <button 
                 type='submit'
                 className='rounded text-white bg-indigo-400 
-                           hover:bg-indigo-900 w-1/2 py-2
-                           flex justify-center' 
-                >
+                         hover:bg-indigo-900 w-1/2 py-2 
+                           flex justify-center'>
                   {isLoading ? <RotatingLines
                     strokeColor="white"
                     strokeWidth="5"
