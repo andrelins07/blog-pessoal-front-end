feb19bfe8a4c44f4917fdef41545f6e611421ca6
Andre Lins <andrelins2403@hotmail.com>
2025-02-04 06:52:00 -0300

    Alterando importação cadastro.css no cadastro.tsx

--- src/pages/cadastro/Cadastro.tsx
+++ src/pages/cadastro/Cadastro.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect, ChangeEvent, FormEvent } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Usuario from '../../models/Usuario'
 import { cadastrarUsuario } from '../../services/Service'
-import './Cadastro.css'
+import './cadastro.css'
 import { RotatingLines } from 'react-loader-spinner'
 import { ToastAlerta } from '../../utils/ToastAlerta'
 
