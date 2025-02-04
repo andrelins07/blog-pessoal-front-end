b2c78ae2305ad50057841d9252dd2f3b8e510420
Andre Lins <andrelins2403@hotmail.com>
2025-02-04 07:07:42 -0300

    Mudando importações cadastro.css

--- src/pages/cadastro/Cadastro.tsx
+++ src/pages/cadastro/Cadastro.tsx
@@ -1,10 +1,10 @@
-import { useState, useEffect, ChangeEvent, FormEvent } from 'react'
-import { useNavigate } from 'react-router-dom'
-import Usuario from '../../models/Usuario'
-import { cadastrarUsuario } from '../../services/Service'
-import { RotatingLines } from 'react-loader-spinner'
-import { ToastAlerta } from '../../utils/ToastAlerta'
-import '../cadastro/Cadastro.css'
+import { useState, useEffect, ChangeEvent, FormEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Usuario from '../../models/Usuario';
+import { cadastrarUsuario } from '../../services/Service';
+import { RotatingLines } from 'react-loader-spinner';
+import { ToastAlerta } from '../../utils/ToastAlerta';
+import './Cadastro.css';
 
 function Cadastro() {
 
